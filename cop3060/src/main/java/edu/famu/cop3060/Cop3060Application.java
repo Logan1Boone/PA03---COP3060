ef1ef4c2dc4cab176cc0ffb23cfb1c3c2797d150
Logan1Boone <logimboone@gmail.com>
2025-12-06 19:16:25 -0500

    create springboot project + create file structure

---
+++ cop3060/src/main/java/edu/famu/cop3060/Cop3060Application.java
@@ -0,0 +1,13 @@
+package edu.famu.cop3060;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cop3060Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cop3060Application.class, args);
+	}
+
+}
---
+++ cop3060/src/test/java/edu/famu/cop3060/resources/ResourceApplicationsTests.java
@@ -0,0 +1,5 @@
+package edu.famu.cop3060.resources;
+
+public class ResourceApplicationsTests {
+    
+}
